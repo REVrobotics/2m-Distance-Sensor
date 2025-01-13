de9a9296c10899c605e6a004e720664090243280
Jan-Felix Abellera <jfabellera@gmail.com>
2025-01-13 16:37:02 -0600

    Add helper script

---
+++ Source/build_and_publish_locally.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+set -e
+
+LIB=DistanceSensor
+BUILD_YEAR=2025
+VENDOR_GENERATED_DIR=./vendordeps/*
+BUILD_REPO_DIR=./build/repos
+RELEASE_DIR=$BUILD_REPO_DIR/releases/com/revrobotics/frc
+MOVE_DIR=C:/Users/Public/wpilib/$BUILD_YEAR/maven/com/revrobotics/frc
+VENDOR_DIR=C:/Users/Public/wpilib/$BUILD_YEAR/vendordeps
+
+echo "*** Building ***"
+./gradlew build -PreleaseMode
+
+echo "*** Generating maven directories ***"
+./gradlew publish -PreleaseMode
+
+echo "*** Ensuring destination maven directories exist ***"
+mkdir -p $MOVE_DIR
+
+echo "*** Moving maven directories ***"
+cp -r $RELEASE_DIR/$LIB-cpp $MOVE_DIR
+cp -r $RELEASE_DIR/$LIB-driver $MOVE_DIR
+cp -r $RELEASE_DIR/$LIB-java $MOVE_DIR
+
+echo "*** Moving vendor deps ***"
+cp -r $VENDOR_GENERATED_DIR $VENDOR_DIR
